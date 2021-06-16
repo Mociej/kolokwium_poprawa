db17a168d214cd5a6c3a97a27f784f4583bfa409
Mociej <maciekkocemba@gmail.com>
2021-06-16 17:12:09 +0200

    add test file

---
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.testreactor.reservation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReservationTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+    @Test
+    void test1(){
+
+    }
+
+}
