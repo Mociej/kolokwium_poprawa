8d32f3bb1fbabe7e4a2ca42ad8e55c17130e8976
Mociej <maciekkocemba@gmail.com>
2021-06-16 17:21:05 +0200

    add setup

--- testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
@@ -1,19 +1,41 @@
 package edu.iis.mto.testreactor.reservation;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReservationTest {
 
+    @Mock
+    Id id;
+
+    Reservation.ReservationStatus status;
+    @Mock
+    ClientData clientData;
+    @Mock
+    Date createDate;
+
+    Reservation reservation;
+
     @BeforeEach
     void setUp(){
-
+        reservation=new Reservation(id,status,clientData,createDate);
     }
+
+    @Test
+    public void itCompiles() {
+        assertThat(true, Matchers.equalTo(true));
+    }
+
     @Test
     void test1(){
 
